e36c38a468dd26dc91815a14ec7f5f2d9b01afc7
MarioBroi <inoix16@gmail.com>
2024-06-13 16:36:51 +0200

    Add slug to TypeSeeden and types table

--- database/migrations/2024_06_13_141755_create_types_table.php
+++ database/migrations/2024_06_13_141755_create_types_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('slug', 150);
             $table->string('icon')->nullable();
             $table->timestamps();
         });
