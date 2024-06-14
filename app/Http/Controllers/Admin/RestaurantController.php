376ad4ed530893d388252b8873bc7851e258040c
MarioBroi <inoix16@gmail.com>
2024-06-14 17:48:44 +0200

    edit create and store methods for restaurants

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -6,6 +6,8 @@ use App\Models\Restaurant;
 use App\Http\Requests\Admin\Restaurant\StoreRestaurantRequest;
 use App\Http\Requests\Admin\Restaurant\UpdateRestaurantRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
 {
@@ -22,7 +24,7 @@ class RestaurantController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.restaurants.create');
     }
 
     /**
@@ -30,7 +32,18 @@ class RestaurantController extends Controller
      */
     public function store(StoreRestaurantRequest $request)
     {
-        //
+        //Validation
+        $val_data = $request->validated();
+
+        //Creating a slug content
+        $slug = Str::slug($val_data['name'], '-');
+        $val_data['slug'] = $slug;
+        $val_data['user_id'] = Auth::id();
+
+        //Creating new istance
+        $restaurant = Restaurant::create($val_data);
+
+        return to_route('admin.restaurants.index')->with('message', "You have created $restaurant->name");
     }
 
     /**
---
+++ resources/views/admin/restaurants/create.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+
+        <div class="d-flex align-items-center justify-content-between">
+            <h1>All Restaurants</h1>
+            <a href="{{ route('admin.restaurants.index') }}" class="btn btn-dark">
+                return to restaurant
+            </a>
+        </div>
+
+        @include('partials.validate')
+
+        <form class="form-control bg-light p-4" action="{{ route('admin.restaurants.store') }}" method="post"
+            enctype="multipart/form-data">
+            @csrf
+
+            <!-- Input for name-->
+            <div class="mb-3">
+                <label for="name" class="form-label">Restaurant name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
+                    aria-describedby="nameHelper" placeholder="name" value="{{ old('name') }}" />
+                @error('name')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <!-- Input for phone number -->
+            <div class="mb-3">
+                <label for="phone_number" class="form-label">Phone number</label>
+                <input type="number" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number"
+                    id="phone_number" aria-describedby="phone_numberHelper" placeholder="phone_number"
+                    value="{{ old('phone_number') }}" />
+                @error('phone_number')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <!-- Input for image-->
+            <div class="mb-3">
+                <label for="image" class="form-label">Image</label>
+                <input type="file" class="form-control @error('image') is-invalid @enderror" name="image"
+                    id="image" aria-describedby="imageHelper" placeholder="image" value="{{ old('image') }}" />
+                @error('image')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <!-- Input for address-->
+            <div class="mb-3">
+                <label for="address" class="form-label">Restaurant address</label>
+                <input type="text" class="form-control @error('address') is-invalid @enderror" name="address"
+                    id="address" aria-describedby="addressHelper" placeholder="address" value="{{ old('address') }}" />
+                @error('address')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <!-- Input for VAT number-->
+            <div class="mb-3">
+                <label for="vat_number" class="form-label">Restaurant vat_number</label>
+                <input type="text" class="form-control @error('vat_number') is-invalid @enderror" name="vat_number"
+                    id="vat_number" aria-describedby="vat_numberHelper" placeholder="vat_number"
+                    value="{{ old('vat_number') }}" />
+                @error('vat_number')
+                    <div class="text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button class="btn btn-primary" type="submit">Add restaurant</button>
+
+        </form>
+    </div>
+@endsection
