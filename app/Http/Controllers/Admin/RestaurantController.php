f9fa5fc374036ca1fb770508c24bff4b1ecdfaa6
MarioBroi <inoix16@gmail.com>
2024-06-14 17:32:12 +0200

    add show method for restaurants

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -38,7 +38,7 @@ class RestaurantController extends Controller
      */
     public function show(Restaurant $restaurant)
     {
-        //
+        return view('admin.restaurants.show', compact('restaurant'));
     }
 
     /**
