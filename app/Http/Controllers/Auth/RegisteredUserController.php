282690eb492ecd8f727f785e567c4152e00680c6
chrialge <chrialge@gmail.com>
2024-06-18 11:29:43 +0200

    edit form of registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -35,7 +37,14 @@ class RegisteredUserController extends Controller
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name_restaurant' => 'required|min:3|max:50',
+            'phone_number' => 'required|min:3|max:20',
+            'address' => 'required',
+            'vat_number' => 'required|min:11|max:11',
         ]);
+
+
+
 
         $user = User::create([
             'name' => $request->name,
@@ -47,6 +56,17 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        if ($request->has('name_restaurant')) {
+            $newRestaurant = new Restaurant();
+            $newRestaurant->name = $request->name_restaurant;
+            $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
+            $newRestaurant->user_id = Auth::id();
+            $newRestaurant->phone_number = $request->phone_number;
+            $newRestaurant->address = $request->address;
+            $newRestaurant->vat_number = $request->vat_number;
+            $newRestaurant->save();
+        }
 
         return redirect(RouteServiceProvider::HOME);
     }
