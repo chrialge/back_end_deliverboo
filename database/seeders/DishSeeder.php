88c35ded6dbf340547ef32e97d97289fa44f24d9
MarioBroi <inoix16@gmail.com>
2024-06-19 10:02:17 +0200

    edit DishSeeder, RestaurantSeeder, User Seeder
    add the foreach cicle to user seeder to generate 20 users, add 1 restaurant to every use, add 1 dish to every restaurant

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -25,28 +25,16 @@ class DishSeeder extends Seeder
             'Zuppa di Pesce', 'Cannoli Siciliani', 'Bistecca alla Fiorentina', 'Gnocchi di Patate'
         ];
 
-        $restaurantId = 1;
-
-        foreach ($dishNames as $name) {
+        foreach ($dishNames as $key => $name) {
             $newDish = new Dish();
             $newDish->name = $name;
             $newDish->slug = Str::slug($name, '-');
-            $newDish->restaurant_id = $restaurantId;
+            $newDish->restaurant_id = $key + 1;
             $newDish->image = $faker->imageUrl(600, 300, 'Dish', true, $name, true, 'jpg');
             $newDish->ingredients = $faker->words(6, true);
             $newDish->price = $faker->randomFloat(2, 5, 20);  // Prezzo compreso tra 5 e 20 euro
             $newDish->visibility = $faker->boolean;
             $newDish->save();
         }
-
-        /* $newDish =  new Dish();
-        $newDish->name = 'Pizza Margherita';
-        $newDish->slug = Str::slug($newDish->name, '-');
-        $newDish->restaurant_id = 1;
-        $newDish->image = $faker->imageUrl(600, 300, 'Dish', true, $newDish->name, true, 'jpg');
-        $newDish->ingredients = "Call'e crabittu, Mazzamineddu, Farina, Acqua, Salsa di pomodoro di Maracalagonis, basilico, Olio EVO DOCG";
-        $newDish->price = 6.50;
-        $newDish->visibility = 0;
-        $newDish->save(); */
     }
 }
--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -32,21 +32,11 @@ class RestaurantSeeder extends Seeder
             $newRestaurant->name = $name;
             $newRestaurant->slug = Str::slug($name, '-');
             $newRestaurant->phone_number = '340' . $faker->randomNumber(7, true);
-            $newRestaurant->user_id = 1;
+            $newRestaurant->user_id = $key + 1;
             $newRestaurant->image = $faker->imageUrl(640, 480, 'food', true, 'restaurant');
             $newRestaurant->address = 'Via ' . $faker->streetName . ' ' . $faker->numberBetween(1, 100);
             $newRestaurant->vat_number = $faker->randomNumber(5, true) . $faker->randomNumber(6, true); //randomNumber ha un massimo d 10 numeri generabili, quindi ne concateno 2 per ottemerme 11. Volendo c'è anche il faker per il VAT italiano ma restituisce anche il IT all'inizio, dovremmo modificare la tabella 
             $newRestaurant->save();
         }
-
-        /* $newRestaurant = new Restaurant();
-        $newRestaurant->name = 'Ossi di seppia';
-        $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->phone_number = '+393200256479';
-        $newRestaurant->user_id = 1;
-        $newRestaurant->image = "https://cdn.blastness.info/media/725/top-Pristhtina/thumbs/full/1600-Food2.jpg";
-        $newRestaurant->address = 'Via della Repubblica 30-32';
-        $newRestaurant->vat_number = $faker->regexify('[A-Za-z0-9]{11}');
-        $newRestaurant->save(); */
     }
 }
