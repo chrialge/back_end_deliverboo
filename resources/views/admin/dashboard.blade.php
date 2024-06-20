89a34e180cd67dc122e56c76cceca500846ee704
MarioBroi <inoix16@gmail.com>
2024-06-20 11:04:08 +0200

    edit dashborad layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,21 +3,27 @@
 @section('content')
     <div class="container">
         <h2 class="fs-4 text-secondary my-4">
-            {{ __('Dashboard') }}
+            Ciao {{ Auth::user()->name }} !
         </h2>
+        <p>Informazioni sul tuo account:</p>
         <div class="row justify-content-center">
             <div class="col">
                 <div class="card">
-                    <div class="card-header">{{ __('#' . $user->id . 'User ' . $user->name . ' ' . $user->last_name) }}</div>
-                    <h1>ciao</h1>
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+                        <ul class="list-unstyled">
+                            <li class="py-1">
+                                Nome: {{ Auth::user()->name }}
+                            </li>
+                            <li class="py-1">
+                                Cognome: {{ Auth::user()->last_name }}
+                            </li>
+                            <li class="py-1">
+                                Indirizzo e-mail: {{ Auth::user()->email }}
+                            </li>
+                            <li class="py-1">
+                                Account creato il: {{ Auth::user()->created_at }}
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
