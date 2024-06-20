16575c0306de6533b71704a01f448224e44b7086
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-06-20 13:15:18 +0200

    add function filter on Api/RestaurantController

--- app/Http/Controllers/API/RestaurantController.php
+++ app/Http/Controllers/API/RestaurantController.php
@@ -37,16 +37,32 @@ class RestaurantController extends Controller
 
     public function filter(Request $request)
     {
-        $types = $request->query('types');
+        //$types = $request->query('types');
+        $types = [1,2];
         // Accesso al primo elemento -> E' un array non ua stringa
-        $firstType = $types[0];
+        //$firstType = $types[0];
 
         //Questa selezione non va bene, errore in console, bisogna fare le join con le altre tabelle
         //$restaurants = Restaurant::with('dishes', 'types')->whereIn('types', $firstType)->first();
 
+        /* SELECT `restaurants`.`name`, `restaurants`.`id` FROM `restaurants` 
+        JOIN `restaurant_type` ON restaurants.id = restaurant_type.restaurant_id 
+        JOIN `types` ON restaurant_type.type_id = types.id
+        WHERE types.id IN (1,3)
+        GROUP BY restaurants.id
+        HAVING COUNT(types.id) = 2; */
+
+        $restaurants = Restaurant::with('types', 'dishes')->select(['restaurants.*'])
+            ->join('restaurant_type', 'restaurants.id', '=', 'restaurant_type.restaurant_id')
+            ->join('types', 'restaurant_type.type_id', '=', 'types.id')
+            ->whereIn('types.id', $types)
+            ->groupBy('restaurants.id')
+            ->havingRaw('COUNT(types.id) =' . count($types))->get();
+
+
         return response()->json([
             'success' => true,
-            'received_data' => $types,
+            'received_data' => $restaurants,
         ]);
     }
 }
