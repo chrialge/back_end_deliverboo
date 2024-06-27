b2bdbf12258e96c32bbc61796a6befec3fbf6af3
chrialge <chrialge@gmail.com>
2024-06-27 11:47:15 +0200

    add icon for types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -16,33 +16,97 @@ class TypeSeeder extends Seeder
     public function run(): void
     {
         $types = [
-            'Pizza', //0
-            'Sushi', //1
-            'Dessert', //2
-            'Poke', //3
-            'Healthy', //4
-            'Hamburger', //5
-            'Kebab', //6
-            'Sandwich', //7
-            'Italiano', //8
-            'Americano', //9
-            'Giapponese', //10
-            'Cinese', //11
-            'Francese', //12
-            'Argentino', //13
-            'Brasiliano', //14
-            'Peruviano', //15
-            'Spagnolo', //16
-            'Portoghese', //17
-            'Thailandese', //18
-            'Libanese', //19
-            'Indiano', //20
+            [
+                "name" => 'Pizza',
+                "icon" => 'icon/pizza.png'
+            ],
+            [
+                "name" => 'Sushi',
+                "icon" => 'icon/sushi.png'
+            ],
+            [
+                "name" => 'Dessert',
+                "icon" => 'icon/dessert.png'
+            ],
+            [
+                "name" => 'Poke',
+                "icon" => 'icon/poke.png'
+            ],
+            [
+                "name" => 'Healthy',
+                "icon" => 'icon/healty.png'
+            ],
+            [
+                "name" => 'Hamburger',
+                "icon" => 'icon/hamburger.png'
+            ],
+            [
+                "name" => 'Kebab',
+                "icon" => 'icon/kebab.png'
+            ],
+            [
+                "name" => 'Sandwich',
+                "icon" => 'icon/sandwich.png'
+            ],
+            [
+                "name" => 'Italiano',
+                "icon" => 'icon/italiano.png'
+            ],
+            [
+                "name" => 'Americano',
+                "icon" => 'icon/americano.png'
+            ],
+            [
+                "name" => 'Giapponese',
+                "icon" => 'icon/giaponese.png'
+            ],
+            [
+                "name" => 'Cinese',
+                "icon" => 'icon/cinese.png'
+            ],
+            [
+                "name" => 'Francese',
+                "icon" => 'icon/francese.png'
+            ],
+            [
+                "name" => 'Argentino',
+                "icon" => 'icon/argentina.png'
+            ],
+            [
+                "name" => 'Brasiliano',
+                "icon" => 'icon/indiano.png'
+            ],
+            [
+                "name" => 'Peruviano',
+                "icon" => 'icon/peruviano.png'
+            ],
+            [
+                "name" => 'Spagnolo',
+                "icon" => 'icon/spagnolo.png'
+            ],
+            [
+                "name" => 'Portoghese',
+                "icon" => 'icon/portoghese.png'
+            ],
+            [
+                "name" => 'Thailandese',
+                "icon" => 'icon/thailandese.png'
+            ],
+            [
+                "name" => 'Libanese',
+                "icon" => 'icon/libanese.png'
+            ],
+            [
+                "name" => 'Indiano',
+                "icon" => 'icon/indiano.png'
+            ],
         ];
 
         foreach ($types as $type) {
             $newType = new Type();
-            $newType->name = $type;
+            $newType->name = $type['name'];
             $newType->slug =  Str::slug($newType->name, '-');
+            $newType->icon = $type['icon'];
             $newType->save();
         };
     }
