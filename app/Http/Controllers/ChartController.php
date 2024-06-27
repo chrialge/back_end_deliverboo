27c9270015188d34935067bfd0ebc471267bfa19
MarioBroi <inoix16@gmail.com>
2024-06-27 16:30:52 +0200

    add data to the chart
    Now we are printing the correct amount of order per month in the chart

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -2,22 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class ChartController extends Controller
 {
     public function index()
     {
+        $user = Auth::id();
+        $restaurant = Restaurant::with('dishes', 'types', 'orders')->where('user_id', $user)->get();
+        //dd($restaurant);
+        $orders = Restaurant::with('dishes', 'types', 'orders')->where('user_id', $user)->get()->pluck('orders')->flatten();;
+        //dd($orders, $restaurant);
+        //dd($orders->first()->created_at);
+
+        $months = [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ];
+
+        foreach ($orders as $order) {
+            //dd($order->created_at->format('m'));
+            for ($i = 0; $i < count($months); $i++) {
+                if (intval($order->created_at->format('m')) == $i + 1) {
+                    $months[$i]++;
+                }
+            }
+        }
+
         $chartjs = app()->chartjs
-            ->name('pieChartTest')
-            ->type('pie')
-            ->size(['width' => 400, 'height' => 200])
-            ->labels(['Label x', 'Label y'])
+            ->name('pieChartTest') /* Nome grafico */
+            ->type('bar') /* tipo di grafico */
+            ->size(['width' => 100, 'height' => 100]) /* dimensioni */
+            ->labels(['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']) /* etichette */
             ->datasets([
                 [
-                    'backgroundColor' => ['#FF6384', '#36A2EB'],
-                    'hoverBackgroundColor' => ['#FF6384', '#36A2EB'],
-                    'data' => [69, 59]
+                    'label' => 'Ordini',
+                    'backgroundColor' => ['#FF6384', '#36A2EB', '#F9CC85'], /* colore lable 1, colore lable 2 */
+                    'hoverBackgroundColor' => ['#FF6384', '#36A2EB', '#F9CC85'],
+                    'data' => $months /* percentuale lable 1,  percentuale lable 1  */
                 ]
             ])
             ->options([]);
