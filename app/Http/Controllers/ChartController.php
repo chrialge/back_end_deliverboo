84278ecc41163fff4e72f765980b7b9a7c6f4f59
chrialge <chrialge@gmail.com>
2024-06-28 02:28:37 +0200

    add query builder

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -2,23 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Carbon;
 use Carbon\Carbon;
 
 class ChartController extends Controller
 {
     public function index(Request $request)
     {
-        dd($request->input('year'));
+        // dd($request->input('year'));
         $user = Auth::id();
         $restaurant = Restaurant::with('dishes', 'types', 'orders')->where('user_id', $user)->get();
         //dd($restaurant);
-        $orders = Restaurant::with('dishes', 'types', 'orders')->where('user_id', $user)->get()->pluck('orders')->flatten();;
+        $orders = Restaurant::with('dishes', 'types', 'orders')->where('user_id', $user)->get()->pluck('orders')->flatten();
         //dd($orders, $restaurant);
         //dd($orders->first()->created_at);
 
+        // dd($restaurant[0]['id']);
+        $restaurant_id = $restaurant[0]['id'];
+        $year = Order::selectRaw('year(created_at) year, monthname(created_at) month, count(*) data')
+            ->where('restaurant_id', $restaurant_id)
+            ->groupBy('year', 'month')
+            ->orderBy('year', 'desc')
+            ->get();
+
+        $cash = Order::selectRaw('year(created_at) anno, monthname(created_at) mese, count(*) orders, sum(total_price) guadagno')
+            ->where('status', '>', 0)
+            ->where('restaurant_id', $restaurant_id)
+            ->groupBy('anno', 'mese')
+            ->orderBy('anno', 'desc')
+            ->get();
+
+        $cash = $cash->filter(function ($val) {
+            return $val['anno'] == 2024;
+        });
+
+        dd($year, $cash);
+        // dd($month);
         $months = [
             0,
             0,
@@ -53,7 +76,7 @@ class ChartController extends Controller
                     'label' => 'Ordini',
                     'backgroundColor' => ['#FF6384', '#36A2EB', '#F9CC85'], /* colore lable 1, colore lable 2 */
                     'hoverBackgroundColor' => ['#FF6384', '#36A2EB', '#F9CC85'],
-                    'data' => $months /* percentuale lable 1,  percentuale lable 1  */
+                    'data' =>  ''/* percentuale lable 1,  percentuale lable 1  */
                 ]
             ])
             ->options([]);
--- resources/views/admin/charts/index.blade.php
+++ resources/views/admin/charts/index.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
     <h1>Grafici</h1>
-
-    <form action="{{ route('admin.charts.index') }}" method="get">
-        @csrf
-        <div class="mb-3">
-            <label for="year" class="form-label">Seleziona un anno</label>
-            <select class="form-select form-select-lg" name="year" id="year">
-                <option value="2024" selected>2024</option>
-                <option value="2023">2023</option>
-                <option value="2022">2022</option>
-                <option value="2021">2021</option>
-            </select>
-        </div>
-        <button type="submit" class="btn btn-primary">
-            Seleziona
-        </button>
-    </form>
+    {{-- 
+    <form action="{{ route('admin.charts.index') }}" method="post"> --}}
+    @csrf
+    <div class="mb-3">
+        <label for="year" class="form-label">Seleziona un anno</label>
+        <select class="form-select form-select-lg" name="year" id="year">
+            <option value="2024" selected>2024</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            <option value="2021">2021</option>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary">
+        Seleziona
+    </button>
+    {{-- </form> --}}
     <!-- /year -->
 
     <div>
