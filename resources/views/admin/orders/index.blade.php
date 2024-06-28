d2f1c6575b822172ecb87f023f0dbb3d51826062
chrialge <chrialge@gmail.com>
2024-06-28 17:50:07 +0200

    add responsive-layout

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
+    <div class="container p-0 p-sm-2">
         <div class="d-flex align-items-center justify-content-between pb-3">
             <h1>Ordini</h1>
         </div>
@@ -19,7 +19,7 @@
 
                         {{-- <th scope="col" class="bg-dark text-white py-3">Indirizzo</th>
                         <th scope="col" class="bg-dark text-white py-3">Telefono</th> --}}
-                        <th scope="col" class="bg-dark text-white py-3 text-end pe-sm-5 pe-1">Azioni</th>
+                        <th scope="col" class="bg-dark text-white py-3 text-end">Azioni</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -29,7 +29,7 @@
                             <td scope="row">{{ $order->customer_name }} {{ $order->customer_lastname }}</td>
                             @foreach ($dateTime = explode(' ', $order->created_at) as $tim)
                                 @if ($loop->index == 0)
-                                    <td>{{ date('d/m/Y', strtotime($order->created_at)) }}</td>
+                                    <td>{{ $dateTime[0] }}</td>
                                 @endif
 
                                 @if ($loop->index == 1)
@@ -37,7 +37,16 @@
                                 @endif
                             @endforeach
 
-                            <td scope="row" class="text-end pe-sm-5 pe-1">
+
+
+                            {{-- <td>{{ $order->customer_address }}</td>
+                            <td>{{ $order->customer_phone_number }}</td>
+                            <td>{{ $order->slug }}</td>
+                            <td>{{ $order->customer_email }}</td>
+                            <td>{{ $order->customer_note }}</td>
+                            <td>{{ $order->total_price }}</td>
+                            <td>{{ $order->status }}</td>  --}}
+                            <td scope="row" class="text-end">
                                 <a href="{{ route('admin.orders.show', $order) }}" class="btn btn-dark">
                                     <i class="fa fa-eye" aria-hidden="true"></i>
                                 </a>
@@ -46,7 +55,7 @@
                     @empty
 
                         <tr class="">
-                            <td colspan="4">Non hai ancora ricevuto nessun ordine! </td>
+                            <td scope="row">Nessun ordine! 😭😭😭😭😭</td>
                         </tr>
                     @endforelse
 
@@ -54,8 +63,6 @@
             </table>
         </div>
 
-
-      {{--  {{ $orders->links('pagination::bootstrap-5') }} --}}
-
+        // {{--  {{ $orders->links('pagination::bootstrap-5') }} --}}
     </div>
 @endsection
